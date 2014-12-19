d74f3ead34a444d9095309fae9fb8e4b045414eb
Dean Moses <moses@tacocat.com>
2014-12-18 20:33:50 -0800

    Migrated 2013.  That's the last one.

--- migrate.js
+++ migrate.js
@@ -3,14 +3,14 @@
 var Walk = require('./scripts/walk.js');
 
 var options = {
-	write: false, // false: don't write files, just log what you WOULD do if you ran it
+	write: true, // false: don't write files, just log what you WOULD do if you ran it
 	logSuccesses: false, // false: don't log successful files.  good for looking at only the failures / weird files
 	logDescriptionIssues: false, // true: log issues with photo and album descriptions like URLs
 	maxPhotosToProcess: 2000 // maximum # of photos to process before quitting
 };
 
-var year = '2006';
-var month = '05';
+var year = '2013';
+var month = '12';
 var day = '30';
 
 //Walk.year(year, options);
