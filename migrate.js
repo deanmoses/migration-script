a82374912df61b6c78337506a08d24514ad209f4
Dean Moses <moses@tacocat.com>
2015-01-02 09:14:14 -0800

    Migrated school halloween subfolder 2014.  This will probably be the last time the script is used.

--- migrate.js
+++ migrate.js
@@ -3,17 +3,17 @@
 var Walk = require('./scripts/walk.js');
 
 var options = {
-	write: true, // false: don't write files, just log what you WOULD do if you ran it
+	write: false, // false: don't write files, just log what you WOULD do if you ran it
 	logSuccesses: false, // false: don't log successful files.  good for looking at only the failures / weird files
 	logDescriptionIssues: false, // true: log issues with photo and album descriptions like URLs
 	warnAboutSubfolders: false, // true: log warning about subfolders on local disk
-	maxPhotosToProcess: 2000 // maximum # of photos to process before quitting
+	maxPhotosToProcess: 200 // maximum # of photos to process before quitting
 };
 
 var year = '2014';
-var month = '12';
-var day = '30';
+var month = '10';
+var day = '31';
 
-//Walk.year(year, options);
-Walk.month(year, month, options);
-//Walk.day(year, month, day, options);
+////Walk.year(year, options);
+//Walk.month(year, month, options);
+Walk.day(year, month, day, options);
